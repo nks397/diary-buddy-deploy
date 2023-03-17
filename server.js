c6e15cc6e191e7ae54e6cbb5c77e9d45253e1eda
nks397 <nkscott97@gmail.com>
2023-03-17 11:17:02 -0400

    Commit all files.

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+// Middleware //
+app.use(express.json())
+app.use(morgan('dev'))
+
+// Connect to DB //
+
+// mongoose.connect('mongodb://localhost:27017/diary-db',
+mongoose.connect(process.env.MONGODB_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log('Connected to the DB')
+)
+
+// Routes //
+app.use('/entries', require('./routes/entryRouter.js'))
+
+// Error handler //
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+
+// Server listen //
+app.listen(5000, () => {
+    console.log('The server is running on port 5000')
+})
